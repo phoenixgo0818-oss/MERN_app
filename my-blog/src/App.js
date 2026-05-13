057ac54b1dac2aa43aa28c37e74d0f57866fcb75
phoenixgo0818-oss <phoenixgo0818@gmail.com>
2026-05-13 19:30:16 +0300

    made the simple home page of the app

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-hero">
+        <h1 className="app-hero__title">KWS Phoenix Blog</h1>
+        <div className="app-hero__rule" aria-hidden="true" />
       </header>
+      <div id="page-body" className="page-body">
+        Welcome to this awesome Blog!
+      </div>
     </div>
   );
 }
